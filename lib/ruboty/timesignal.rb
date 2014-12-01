eef8e75aefcd92a51eb318a42e464638e9fe404f
Tatsuya Toyoda <b8eansatz@gmail.com>
2014-12-01 21:11:50 +0900

    Correct algorithm

--- lib/ruboty/timesignal.rb
+++ lib/ruboty/timesignal.rb
@@ -1,14 +1,33 @@
 module Ruboty
   module TimeSignal
     class TimeSignal < Base
-      loop {
+      def timer
         now = Time.now
-        if now.sec == 0 && now.min == 0
-          SlackBot.notify (
-	    body: "It's #{now.hour} o'clock now !"
-	  )
+      	alert_time = now.hour + 1
+      	alert_time = alert_time.modulo(24)
+      	min = 60 - now.min
+      	sec = 60 - now.sec
+
+      	sleep sec
+
+      	while min > 0 do
+	  min -= 1
+	  #if min == 30
+	  #  puts "It's #{now.hour}:30 now !"
+	  #end
+	  #puts "#{min} minutes to go"
+	  sleep 60
       	end
-      }
+    
+	loop {
+          SlackBot.notify (
+            body: "It's #{alert_time} o'clock now !"
+	  )
+	  alert_time += 1
+	  alert_time = alert_time.modulo(24)
+	  sleep 3600
+      	}
+      end
     end
   end
 end
